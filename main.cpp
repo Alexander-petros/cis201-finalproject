b0019e6449e3f8cad53ad39b3691813045844072
alexander petros <alexanderpetros@ac5r2c2.cis>
2017-12-07 11:31:57 -0800

    isolates words and removes duplicaes

--- main.cpp
+++ main.cpp
@@ -8,26 +8,52 @@ using namespace std;
 class Words
 {
 public:
-    void set_string(string s)
+    Words(vector<char> c)
     {
-        word = s;
+       set_string(c);
+       add_count();
+    }
+    void set_string(vector<char> s)
+    {
+       string x(s.data(), s.size());
+        word = x;
     }
     void add_count()
     {
         count++;
+    }
+    char check_char(int i)
+    {
+       return word.at(i);
+    }
+    string return_word()
+    {
+       return word;
     }
 
 
 private:
     string word;
     int count;
-    vector<int> at_lines;
+    vector<int> at_line;
 };
+bool check_word (vector<Words> w, vector<char> word)
+{
+   return false;
+}
+void read_all (vector<Words> w)
+{
+   for (int i = 0; i <=  w.size(); i++)
+   {
+      cout << w[i].return_word() << endl;
+   }
+}
 int main ()
 {
     string line;
     char buffer;
-    Words word;
+    bool new_word = true;
+    int wordcount = 0;
     vector<Words> w;
     vector<char> c;
     int line_count = 0;
@@ -38,9 +64,45 @@ int main ()
     {
         while (alice.get(buffer) )
         {
-            c.push_back(tolower(buffer));
-            cout << c[num] << '\n';
-            num++;
+           if ( (buffer <= 47 && buffer != 32) || (buffer >= 94 && buffer <= 96) || (buffer >= 58 && buffer <= 64))
+              //Detects if it is any punctuation and ignores it
+           {
+           }
+           else
+           {
+               if (buffer != 32)// && check_word(w,c) == true)
+               {
+                 c.push_back(tolower(buffer));
+                 //cout << c[num];
+                 num++;
+               }
+               else 
+               {
+                Words * word = new Words(c);
+                //cout << word->return_word() << endl;
+                c.clear();
+                for ( int i = 0; i < w.size(); i++)
+                {
+                   if ( w[i].return_word() == word->return_word())
+                      {
+                         new_word = false;
+                         cout << "Old Word" << endl;
+                         delete word;
+                         break;
+                      }
+                   else
+                   {
+                      new_word = true;
+                   }
+                }
+                if (new_word == true)
+                {
+                   cout << "New Word" << word->return_word() << endl;
+                   w.push_back(*word);
+                }
+                num = 0;
+              }
+            }
         }
         alice.close();
     }
@@ -48,6 +110,6 @@ int main ()
     {
         cout << "Unable to open file";
     }
-    
+    read_all(w);
     return 0;
 }
